7f4c68ab7d009ff2899fe10c98b911d3a33d6a22
Vvramen <ramensky.exceedteam@google.com>
2020-10-28 11:21:52 +0300

    Revert "task solved"
    This reverts commit 0ff1cd1cc3bdffcbd83a9c70e65afdcf2cba91e9.

--- script.js
+++ script.js
@@ -1,57 +1,18 @@
-let id = 1;
-const array = ['color-blue', 'color-yellow', 'color-green', 'color-red', 'color-salad', 'color-deep-blue'];
+const button = document.querySelector(".btn");
+const form = document.querySelector(".form");
 
-function loadPage (){
-    const mainButton = document.getElementById('main');
-    mainButton.addEventListener('click', createTask);
-}
-
-function createTask() {
-    const value = document.getElementById('input').value;
-    if (!value) return (alert("Неужели нет никаких планов!?"));
-
-    const checkColor = document.querySelector('input.color:checked');
-    const div = document.createElement('div');
-    const color = checkColor ? checkColor.classList[1] : array[Math.floor(Math.random() * array.length)];
-
-    div.id = id + "item";
-    div.classList.add("container", color)
-
-    const divCheck = document.createElement("div");
-    divCheck.id = "div-check" + id;
-    divCheck.className = "check";
-
-    const divText = document.createElement("div");
-    divText.id = "div-text" + id;
-    divText.className = "text";
-
-    const checkbox = document.createElement("input");
-    checkbox.type = "checkbox";
-    checkbox.id = id;
-    const label = document.createElement("label");
-    label.innerText = value;
-
-    divCheck.appendChild(checkbox);
-    divText.appendChild(label);
-
-    div.appendChild(divCheck);
-    div.appendChild(divText);
-
-    document.getElementById('list').appendChild(div);
-    checkbox.addEventListener('click', completed);
-
-    if (checkColor) checkColor.checked = false;
-    document.getElementById('input').value = "";
-    id++;
-}
-
-function completed(e) {
-    const id = e.target.id;
-    const div = document.getElementById(id + "item");
-
-    if (e.target.checked) {
-        div.classList.add("completed-task");
+button.addEventListener("click", function (e) {
+    e.preventDefault();
+    const newText = document.createElement("input");
+    newText.type = "text";
+    newText.classList.add("text");
+    if (form[form.length - 2].value.indexOf("555") !== -1) {
+        newText.value = form[form.length - 2].value.replace(/555/g,"---");
+    } else if ((form.length - 2) % 2 === 0){
+        newText.value = form[form.length - 2].value.toUpperCase();
     } else {
-        div.classList.remove("completed-task");
+        newText.value = form[form.length - 2].value.toLowerCase();
     }
-}
+
+    button.before(newText);
+})
